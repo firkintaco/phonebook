37042125b86a2ab590c64f91bd8645b73046223e
firkintaco <firkintaco@gmail.com>
2023-06-13 15:18:01 +0300

    Testing mongo validation

--- models/person.js
+++ models/person.js
@@ -13,7 +13,17 @@ mongoose
 // MongoDB Schema
 const personSchema = new mongoose.Schema({
   name: { type: String, required: true, minlength: 4 },
-  number: String,
+  number: {
+    type: String,
+    minlength: 8,
+    validate: {
+      validator: function (v) {
+        return /\d{3}-\d{7}/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+    required: [true, "phone number required"],
+  },
 });
 
 personSchema.set("toJSON", {
